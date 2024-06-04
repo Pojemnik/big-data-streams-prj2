34ac353d34f1b38a6f4eb3a8d7462a182048dd94
unknown <andrzej.gauza@outlook.com>
2024-06-04 18:34:53 +0200

    Cleanup process.py

--- process.py
+++ process.py
@@ -1,127 +1,148 @@
 import socket
+import sys
+from pyspark.sql import SparkSession
 from pyspark.sql.functions import expr, split, window, count, sum, approx_count_distinct, date_format, col, concat, lit
 from pyspark.sql.types import StructType, TimestampType, StringType, IntegerType
 from pyspark.sql import DataFrame
 
-host_name = socket.gethostname()
+def process_realtime_data(data: DataFrame, movies: DataFrame, host_name: str, mode: str):
+    if mode == 'a':
+        #Realtime mode A
+        win = data.groupBy(
+            window("date", "30 days"), data.film_id) \
+            .agg(
+                count("rate").alias("rate_count"),
+                sum("rate").alias("rate_sum"),
+                approx_count_distinct("user_id").alias("user_count")
+            )
+        win = win.withColumn("month", date_format(win.window.start, "MM.yyyy"))
+        win = win.join(movies, movies.ID == win.film_id) \
+        .withColumnRenamed("Title", "title") \
+        .withColumn("key", concat(col("film_id"), lit(","), col("month")))
+        win = win.drop("window", "ID", "Year")
 
-source = spark.readStream \
-.format("kafka") \
-.option("kafka.bootstrap.servers", f"{host_name}:9092") \
-.option("subscribe", "prj-2-input") \
-.load()
-data = source.select(expr("CAST(value AS STRING)").alias("value"))
+        #Write to Redis
+        query = win.writeStream \
+        .outputMode("update") \
+        .foreachBatch (
+            lambda batchDF, _:
+            batchDF.write
+                .mode("append") \
+                .format("org.apache.spark.sql.redis") \
+                .option("table", "result") \
+                .option("key.column", "key") \
+                .option("host", host_name) \
+                .save()
+        ) \
+        .start()
+    
+    elif mode == 'c':
+        #Realtime mode C
+        win = data.withWatermark("date", "30 days") \
+            .groupBy(
+                window("date", "30 days"), data.film_id) \
+                .agg(count("rate").alias("rate_count"),
+                sum("rate").alias("rate_sum"),
+                approx_count_distinct("user_id").alias("user_count")
+            )
+        win = win.withColumn("month", date_format(win.window.start, "MM.yyyy"))
+        win = win.join(movies, movies.ID == win.film_id) \
+        .withColumnRenamed("Title", "title") \
+        .withColumn("key", concat(col("film_id"), lit(","), col("month")))
+        win = win.drop("window", "ID", "Year")
 
-split_columns = split(data["value"], ",")
+        #Write to Redis
+        query = win.writeStream \
+        .outputMode("append") \
+        .foreachBatch (
+            lambda batchDF, _:
+            batchDF.write
+                .mode("append") \
+                .format("org.apache.spark.sql.redis") \
+                .option("table", "result") \
+                .option("key.column", "key") \
+                .option("host", host_name) \
+                .save()
+        ) \
+        .start()
 
-data = data.withColumn("date", split_columns[0].cast(TimestampType())) \
-.withColumn("film_id", split_columns[1]) \
-.withColumn("user_id", split_columns[2]) \
-.withColumn("rate", split_columns[3].cast(IntegerType()))
-data = data.drop("value")
-
-movies = spark.read.option("header", True).csv("gs://pojemnik/projekt2/movie_titles.csv")
-
-# Tryb A
-win = data.groupBy(
-    window("date", "30 days"), data.film_id) \
+def detect_anomalies(data: DataFrame, movies: DataFrame, host_name: str):
+    anomaly_window_length = 30
+    anomaly_min_rate_count = 2
+    anomaly_min_avg_rate = 3
+    anomalies_window = data.withWatermark("date", f"{anomaly_window_length} days") \
+    .groupBy(window("date", f"{anomaly_window_length} days", "1 day"), data.film_id) \
     .agg(
         count("rate").alias("rate_count"),
-        sum("rate").alias("rate_sum"),
-        approx_count_distinct("user_id").alias("user_count")
+        sum("rate").alias("rate_sum")) \
+    .select(
+        col("film_id"),
+        col("rate_count"),
+        date_format(col("window").start, "dd.MM.yyyy").alias("window_start"),
+        date_format(col("window").end, "dd.MM.yyyy").alias("window_end"),
+        (col("rate_sum") / col("rate_count")).alias("avg_rate"),
+        col("rate_count")
     )
-win = win.withColumn("month", date_format(win.window.start, "MM.yyyy"))
-win = win.join(movies, movies.ID == win.film_id) \
-.withColumnRenamed("Title", "title") \
-.withColumn("key", concat(col("film_id"), lit(","), col("month")))
-win = win.drop("window", "ID", "Year")
 
-query = win.writeStream \
-.outputMode("update") \
-.foreachBatch (
-    lambda batchDF, _:
-    batchDF.write
-        .mode("append") \
-        .format("org.apache.spark.sql.redis") \
-        .option("table", "result") \
-        .option("key.column", "key") \
-        .option("host", host_name) \
-        .save()
-) \
-.start()
+    anomalies = anomalies_window.where(
+        (anomalies_window.rate_count > anomaly_min_rate_count) &
+        (anomalies_window.avg_rate > anomaly_min_avg_rate)
+    ).join(movies, movies.ID == anomalies_window.film_id) \
+    .drop("ID", "Year", "film_id")
 
-# Tryb C
-win = data.withWatermark("date", "30 days") \
-    .groupBy(
-        window("date", "30 days"), data.film_id) \
-        .agg(count("rate").alias("rate_count"),
-        sum("rate").alias("rate_sum"),
-        approx_count_distinct("user_id").alias("user_count")
-    )
-win = win.withColumn("month", date_format(win.window.start, "MM.yyyy"))
-win = win.join(movies, movies.ID == win.film_id) \
-.withColumnRenamed("Title", "title") \
-.withColumn("key", concat(col("film_id"), lit(","), col("month")))
-win = win.drop("window", "ID", "Year")
+    #Format results for Kafka output
+    anomalies_formatted = anomalies.select(concat(
+        col("window_start"),
+        lit(","),
+        col("window_end"),
+        lit(","),
+        col("Title"),
+        lit(","),
+        col("rate_count"),
+        lit(","),
+        col("avg_rate"),
+    ).alias("value"))
 
-query = win.writeStream \
-.outputMode("append") \
-.foreachBatch (
-    lambda batchDF, _:
-    batchDF.write
-        .mode("append") \
-        .format("org.apache.spark.sql.redis") \
-        .option("table", "result") \
-        .option("key.column", "key") \
-        .option("host", host_name) \
-        .save()
-) \
-.start()
+    anomalies_output = anomalies_formatted.writeStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", f"{host_name}:9092") \
+    .option("topic", "prj-2-anomalies") \
+    .option("checkpointLocation", "/tmp/anomaly_checkpoints/") \
+    .start()
 
-# Anomalie
-anomaly_window_length = 30
-anomaly_min_rate_count = 2
-anomaly_min_avg_rate = 3
-anomalies_window = data.withWatermark("date", f"{anomaly_window_length} days") \
-.groupBy(window("date", f"{anomaly_window_length} days", "1 day"), data.film_id) \
-.agg(
-    count("rate").alias("rate_count"),
-    sum("rate").alias("rate_sum")) \
-.select(
-    col("film_id"),
-    col("rate_count"),
-    date_format(col("window").start, "dd.MM.yyyy").alias("window_start"),
-    date_format(col("window").end, "dd.MM.yyyy").alias("window_end"),
-    (col("rate_sum") / col("rate_count")).alias("avg_rate"),
-    col("rate_count")
-)
 
-anomalies = anomalies_window.where(
-    (anomalies_window.rate_count > anomaly_min_rate_count) &
-    (anomalies_window.avg_rate > anomaly_min_avg_rate)
-).join(movies, movies.ID == anomalies_window.film_id) \
-.drop("ID", "Year", "film_id")
+def main():
+    mode = sys.argv[1]
 
-anomalies_formatted = anomalies.select(concat(
-    col("window_start"),
-    lit(","),
-    col("window_end"),
-    lit(","),
-    col("Title"),
-    lit(","),
-    col("rate_count"),
-    lit(","),
-    col("avg_rate"),
-).alias("value"))
+    spark = SparkSession.builder \
+    .appName("BigData Netflix") \
+    .getOrCreate()
+    host_name = socket.gethostname()
 
-query = win.writeStream \
-.format("console") \
-.outputMode("complete") \
-.start()
+    #Load data
+    source = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", f"{host_name}:9092") \
+    .option("subscribe", "prj-2-input") \
+    .load()
+    data = source.select(expr("CAST(value AS STRING)").alias("value"))
 
-anomalies_output = anomalies_formatted.writeStream \
-.format("kafka") \
-.option("kafka.bootstrap.servers", f"{host_name}:9092") \
-.option("topic", "prj-2-anomalies") \
-.option("checkpointLocation", "/tmp/anomaly_checkpoints/") \
-.start()
+    movies = spark.read.option("header", True).csv("gs://pojemnik/projekt2/movie_titles.csv")
+
+    #Format and clean input
+    split_columns = split(data["value"], ",")
+
+    data = data.withColumn("date", split_columns[0].cast(TimestampType())) \
+    .withColumn("film_id", split_columns[1]) \
+    .withColumn("user_id", split_columns[2]) \
+    .withColumn("rate", split_columns[3].cast(IntegerType()))
+    data = data.drop("value")
+
+    process_realtime_data(data, movies, host_name, mode)
+
+    detect_anomalies(data, movies, host_name)
+
+    spark.streams.awaitAnyTermination()
+
+if __name__ == "__main__":
+    main()
